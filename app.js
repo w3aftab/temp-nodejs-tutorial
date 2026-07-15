39858d015279d4f6a0754634171fea992a0bc00e
Aftab Ahmed <w3aftab@gmail.com>
2026-07-15 23:59:40 +0600

    modified

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 import { readFile } from "fs";
 
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf8", (err, data) => {
-      if (err) reject(err);
-      console.log(data);
-    });
-  });
-};
-
-getText(`./content/first.txt`)
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err));
+readFile("./content/first.txt", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
